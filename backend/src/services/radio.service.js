23e32c4833998a7a63a0e646e3dadbccaffe3d1e
Lostcrown301 <lostcrown301@gmail.com>
2026-07-05 12:28:14 +0530

    Resolved the backend issue where mostly error 500 was getting returned

--- backend/src/services/radio.service.js
+++ backend/src/services/radio.service.js
@@ -3,14 +3,23 @@ import util from 'util';
 import axios from 'axios';
 import fs from "fs/promises";
 
-const resolveSrv = util.promisify(dns.resolveSrv);
+const resolve4 = util.promisify(dns.resolve4);
+const reverse = util.promisify(dns.reverse);
 
 export async function fetchRandomStation() {
     const workingUrl = await getWorkingRadioBrowserUrl();
 
+    console.log(`using ${workingUrl}`);
+
     // Get all countries
     const countriesResponse = await axios.get(
-        `${workingUrl}/json/countries`
+        `${workingUrl}/json/countries`,
+        {
+            timeout: 5000,
+            headers: {
+                "User-Agent": "RadioHunt/1.0"
+            }
+        }
     );
 
     const countries = countriesResponse.data;
@@ -29,13 +38,16 @@ export async function fetchRandomStation() {
     const validCountries = countries.filter(country => country.stationcount >= 50);
 
     // Pick random country
-    const country =
-        validCountries[Math.floor(Math.random() * validCountries.length)];
+    const country = validCountries[Math.floor(Math.random() * validCountries.length)];
 
     // Get stations from that country
     const stationsResponse = await axios.get(
         `${workingUrl}/json/stations/bycountry/${encodeURIComponent(country.name)}`,
-        {
+        {   
+            timeout: 5000,
+            headers: {
+                "User-Agent": "RadioHunt/1.0"
+            },
             params: {
                 hidebroken: true
             }
@@ -49,20 +61,33 @@ export async function fetchRandomStation() {
     }
 
     // Pick random station
-    return stations[
-        Math.floor(Math.random() * stations.length)
-    ];
+    return stations[Math.floor(Math.random() * stations.length)];
 }
 
 /**
  * Get a list of base urls of all available radio-browser servers
  * Returns: array of strings - base urls of radio-browser servers
  */
-function get_radiobrowser_base_urls() {
-    return resolveSrv("_api._tcp.radio-browser.info").then(hosts => {
-        hosts.sort();
-        return hosts.map(host => "https://" + host.name);
-    });
+async function get_radiobrowser_base_urls() {
+    const ips = await resolve4("all.api.radio-browser.info");
+
+    const reverseLookups = await Promise.allSettled(
+        ips.map(ip => reverse(ip))
+    );
+
+    const hosts = reverseLookups
+        .filter(result => result.status === "fulfilled")
+        .map(result => `https://${result.value[0]}`);
+
+    // Shuffle (recommended by Radio Browser)
+    for (let i = hosts.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [hosts[i], hosts[j]] = [hosts[j], hosts[i]];
+    }
+
+    console.log("Available mirrors:", hosts);
+
+    return hosts;
 }
 
 async function getWorkingRadioBrowserUrl() {
@@ -71,10 +96,17 @@ async function getWorkingRadioBrowserUrl() {
     for (const host of hosts) {
         try {
 //            console.log(host);
-            await axios.get(`${host}/json/stations`, {
-                timeout: 5000,
-                params: { limit: 1 }
-            });
+            await axios.get(`${host}/json/stations`,
+                {
+                    timeout: 5000,
+                    headers: {
+                        "User-Agent": "RadioHunt/1.0"
+                    },
+                    params: {
+                        limit: 1
+                    }
+                }
+            );
 
             console.log(`Using mirror: ${host}`);
             return host;
